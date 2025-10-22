95a55327f02f10996eb2d49d00d6e77f41678ed8
RileyMwork <rileywmartin117@gmail.com>
2025-10-22 12:09:06 -0400

    Initial Commit with passing Tests for Get Method

---
+++ project1/model/Post.cs
@@ -0,0 +1,10 @@
+namespace project1.model
+{
+    public class Post
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
---
+++ project1/test/post/GetTests.cs
@@ -0,0 +1,40 @@
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+using project1.model;
+using System.Reflection;
+
+namespace project1.test.post {
+    public class GetTests {
+        private readonly HttpClient _client;
+
+        public GetTests() {
+            _client = new HttpClient{
+                BaseAddress = new Uri("https://jsonplaceholder.typicode.com/")
+            };
+        }
+
+        [Fact]
+        public async Task getAllPostsReturnsSuccessAndList() {
+            var response = await _client.GetAsync("posts");
+            response.EnsureSuccessStatusCode();
+
+            var posts = await response.Content.ReadFromJsonAsync<List<Post>>();
+            Assert.NotNull(posts);
+            Assert.NotEmpty(posts);
+            Assert.True(posts.Count >= 2);
+        }
+
+        [Fact]
+        public async Task getPostByIdReturnsSuccessAndCorrectId()
+        {
+            var response = await _client.GetAsync("posts/1");
+            response.EnsureSuccessStatusCode();
+
+            var post = await response.Content.ReadFromJsonAsync<Post>();
+            Assert.NotNull(post);
+            Assert.Equal(1, post.Id);
+        }
+
+    }
+}
